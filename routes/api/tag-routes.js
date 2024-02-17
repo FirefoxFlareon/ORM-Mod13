8bf26bec576b10216c8579cac4bf76ca4352fc7a
Firefox <tshankgg@gmail.com>
2024-02-17 12:48:10 -0600

    finished tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,16 +41,42 @@ router.post('/', async (req, res) => {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }});
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag ID not found'});
+        return;
+      };
+      res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }});
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag ID not found'});
+        return;
+      };
+      res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
